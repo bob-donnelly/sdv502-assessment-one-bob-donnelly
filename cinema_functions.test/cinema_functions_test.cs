bb453576774848d22b62c7c5d6b814e61821a6b1
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-09-08 06:24:16 +1200

    Finished Family Pass test cases.

--- cinema_functions.test/cinema_functions_test.cs
+++ cinema_functions.test/cinema_functions_test.cs
@@ -36,12 +36,12 @@ namespace cinema_functions.test
             // Act 
             // Action that is taken is that the price is calculated from the arguments passed into the method called from cinema_functions.cs file.
 
-            decimal price = TicketPrice.Adult_Before_5(pr_quantity, pr_person, pr_day, pr_time);
+            decimal ticketPrice = TicketPrice.Adult_Before_5(pr_quantity, pr_person, pr_day, pr_time);
 
             // Assert
             // Asserting the price is equal to the result or the expected result we have filled out using That method comparing truthy values.
 
-            Assert.That(price == result);
+            Assert.That(ticketPrice == result);
         }
 
         [TestCase(1, "adult", "Monday", 7, 17.50)]
@@ -92,7 +92,13 @@ namespace cinema_functions.test
             Assert.That(ticketPrice == result);
         }
 
-        [TestCase(1, "Adult", 12)]
+        [TestCase(1, "child", 12)]
+        [TestCase(3, "Child", 36)]
+        [TestCase(0, "child", -1)]
+        [TestCase(-1, "child", -1)]
+        [TestCase(1, "Adult", -1)]
+        [TestCase(1, "Student", -1)]
+        [TestCase(1, "Senior", -1)]
 
         public void Child_Under_16_Expect_12_Per_Person(int pr_quantity, string pr_person, decimal result)
         {
@@ -107,7 +113,76 @@ namespace cinema_functions.test
             // Assert
 
             Assert.That(ticketPrice == result);
+        }
 
+        [TestCase(1, "Senior", 12.50)]
+        [TestCase(2, "Senior", 25)]
+        [TestCase(0, "Senior", -1)]
+        [TestCase(-1, "Senior", -1)]
+        [TestCase(1, "Adult", -1)]
+        [TestCase(1, "Student", -1)]
+        [TestCase(1, "Child", -1)]
+        public void Senior_Expect_12_50_Per_Person(int pr_quantity, string pr_person, decimal result)
+        {
+            //Arrange
+
+            TicketPriceController TicketPrice = new TicketPriceController();
+
+            // Act
+
+            decimal ticketPrice = TicketPrice.Senior(pr_quantity, pr_person);
+
+            // Assert
+
+            Assert.That(ticketPrice == result);
+        }
+
+        [TestCase(1, "Student", 14)]
+        [TestCase(4, "Student", 56)]
+        [TestCase(0, "Student", -1)]
+        [TestCase(-1, "Student", -1)]
+        [TestCase(1, "Adult", -1)]
+        [TestCase(1, "Child", -1)]
+        [TestCase(1, "Senior", -1)]
+
+        public void Student_Expect_14_Per_Person(int pr_quantity, string pr_person, decimal result)
+        {
+            //Arrange
+
+            TicketPriceController TicketPrice = new TicketPriceController();
+
+            // Act
+
+            decimal ticketPrice = TicketPrice.Student(pr_quantity, pr_person);
+
+            // Assert
+
+            Assert.That(ticketPrice == result);
+        }
+
+        [TestCase(1, 2, 2, 46)]
+        [TestCase(1, 1, 3, 46)]
+        [TestCase(4, 2, 2, 184)]
+        [TestCase(2, 1, 3, 92)]
+        [TestCase(2, 3, 5, 92)]
+        [TestCase(0, 2, 2, -1)]
+        [TestCase(-1, 1, 3, -1)]
+        [TestCase(2, 2, 6, -1)]
+        [TestCase(1, "adult", "senior", -1)]
+
+        public void Family_Pass_Expect_46_All_Persons(int pr_quantity_ticket, int pr_quantity_adult, int pr_quantity_child, decimal result)
+        {
+            //Arrange
+
+            TicketPriceController TicketPrice = new TicketPriceController();
+
+            // Act
+
+            decimal ticketPrice = TicketPrice.Family_Pass(pr_quantity_ticket, pr_quantity_adult, pr_quantity_child);
+
+            // Assert
+
+            Assert.That(ticketPrice == result);
         }
     }
 }
