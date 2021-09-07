afe7c0c3b8edb7cac86db4baeca99e1cc4f80e7c
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-09-07 17:03:52 +1200

    Saved and completed Adult before and after 5 test cases.

--- cinema_functions.test/cinema_functions_test.cs
+++ cinema_functions.test/cinema_functions_test.cs
@@ -1,15 +1,48 @@
 ﻿using System;
+using cinema_functions;
 using NUnit.Framework;
 
 namespace cinema_functions.test
 {
+
     [TestFixture]
-    public class cinemaTests
+
+    public class TicketPriceControllerTests
     {
-        [Test]
-        public void test()
+
+        [TestCase(1, "adult", "Monday", 3, 14.50)]
+        [TestCase(5, "adult", "Wednesday", 4, 72.5)]
+        [TestCase(1, "adult", "Tuesday", 2, -1)]
+        [TestCase(0, "adult", "Friday", 4, -1)]
+        [TestCase(-1, "adult", "Sunday", 1, -1)]
+
+        public void Adult_Before_5_Expect_14_50_Per_Person(int pr_quantity, string pr_person, string pr_day, decimal pr_time, decimal result)
         {
-            Assert.That(1 == 0);
+            // Arrange
+
+            TicketPriceController TicketPrice = new TicketPriceController();
+
+            // Act 
+
+            decimal price = TicketPrice.Adult_Before_5(pr_quantity, pr_person, pr_day, pr_time);
+
+            // Assert
+
+            Assert.That(price == result);
+        }
+
+        [TestCase(1, "adult", "Monday", 7, 17.50)]
+        [TestCase(3, "adult", "Wednesday", 6, 52.5)]
+        [TestCase(1, "adult", "Tuesday", 6, -1)]
+        [TestCase(0, "adult", "Thursday", 7, -1)]
+        [TestCase(-1, "adult", "Sunday", 7, -1)]
+        public void Adult_After_5_Expect_17_50_Per_Person(int pr_quantity, string pr_person, string pr_day, decimal pr_time, decimal result)
+        {
+            TicketPriceController TicketPrice = new TicketPriceController();
+
+            decimal ticketPrice = TicketPrice.Adult_After_5(pr_quantity, pr_person, pr_day, pr_time);
+
+            Assert.That(ticketPrice == result);
         }
     }
 }
