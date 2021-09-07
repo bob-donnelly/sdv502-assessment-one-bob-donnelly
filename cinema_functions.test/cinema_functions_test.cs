355d3796f99cdc74bd16f355744420846fd901db
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2021-09-07 12:29:21 +1200

    Set up repository with cinema_functions and Nunit test framework.

---
+++ cinema_functions.test/cinema_functions_test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace cinema_functions.test
+{
+    [TestFixture]
+    public class cinemaTests
+    {
+        [Test]
+        public void test()
+        {
+            Assert.That(1 == 0);
+        }
+    }
+}
